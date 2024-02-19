4da3c457cfd38778e638b26db93f3a882a6cd676
fernandofx <fdelaflorfx@gmail.com>
2024-02-18 21:47:03 -0500

    reportes PDF y Excel

--- src/main/java/com/fer/gestioncursos/controller/CursoController.java
+++ src/main/java/com/fer/gestioncursos/controller/CursoController.java
@@ -2,7 +2,10 @@ package com.fer.gestioncursos.controller;
 
 
 import com.fer.gestioncursos.entity.Curso;
+import com.fer.gestioncursos.reports.CursoExporterExcel;
+import com.fer.gestioncursos.reports.CursoExporterPDF;
 import com.fer.gestioncursos.repository.CursoRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -83,6 +90,43 @@ public class CursoController {
         return "redirect:/cursos";
     }
 
+    @GetMapping("/export/pdf")
+    public void generarReportePdf(HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/pdf");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormat.format(new Date());
+        String headerKey = "Content-Disposition";
+
+        String headerKeyValue = "attachment; filename=cursos" + currentDateTime + ".pdf";
+
+        response.setHeader(headerKey, headerKeyValue);
+
+        List<Curso> cursos = cursoRepository.findAll();
+
+        CursoExporterPDF exporterPDF = new CursoExporterPDF(cursos);
+        exporterPDF.export(response);
+
+    }
+
+    @GetMapping("/export/excel")
+    public void generarReporteExcel(HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/octet-stream");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormat.format(new Date());
+        String headerKey = "Content-Disposition";
+
+        String headerKeyValue = "attachment; filename=cursos" + currentDateTime + ".xlsx";
+
+        response.setHeader(headerKey, headerKeyValue);
+
+        List<Curso> cursos = cursoRepository.findAll();
+
+        CursoExporterExcel exporterExcel = new CursoExporterExcel(cursos);
+        exporterExcel.export(response);
+
+    }
 
 
 }
